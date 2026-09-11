7876b9d14532a692e7602471bd57458788fd0435
Kia <kiabrow98@gmail.com>
2026-09-11 00:15:56 -0500

    Finished rockpaperscissors

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -12,16 +12,22 @@ function showRockButton () {
     const computersRoll = generateComputersRoll();
     getResult.value = "You chose Rock!" + "             ";
     getResult.value += `Computer chose ${computersRoll}!`;
+    const winner = gameWinner("Rock", computersRoll);
+    getResult.value += ` ${winner}`;
 }
 function showPaperButton () {
     const computersRoll = generateComputersRoll();
     getResult.value = "You chose Paper!" + "             ";
     getResult.value += `Computer chose ${computersRoll}!`;
+    const winner = gameWinner("Paper", computersRoll);
+    getResult.value += ` ${winner}`;
 }
 function showScissorsButton () {
     const computersRoll = generateComputersRoll();
     getResult.value = "You chose Scissors!" + "             ";
     getResult.value += `Computer chose ${computersRoll}!`;
+    const winner = gameWinner("Scissors", computersRoll);
+    getResult.value += ` ${winner}`;
 }
 
 function generateComputersRoll () {
@@ -32,5 +38,18 @@ function generateComputersRoll () {
         return "Paper";
     } else {
         return "Scissors";
+    }
+}
+function gameWinner (playersChoice, computersChoice) {
+    if (playersChoice === computersChoice) {
+        return "             " + "Its a tie!";
+    } else if (playersChoice === "Rock" && computersChoice === "Scissors") {
+        return "             " + "You Win!";
+    } else if (playersChoice === "Paper" && computersChoice === "Rock") {
+        return "             " + "You Win!";
+    } else if (playersChoice === "Scissors" && computersChoice === "Paper") {
+        return "You Win!";
+    } else {
+        return "             " +"Computer Wins!";
     }
 }
