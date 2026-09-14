f1aeec7698d2bcf38a6178d772894cea4e8d4ff4
Kia <kiabrow98@gmail.com>
2026-09-13 20:09:27 -0500

    8ball html and css done

---
+++ 8Ball.js
@@ -0,0 +1,4 @@
+const getQuestionInput = document.querySelector("#8-ball-question");
+const pressShakeButton = document.querySelector("#shake-button");
+const getResultsInput = document.querySelector("#result");
+
